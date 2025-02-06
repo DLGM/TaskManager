790b49f70bcd63340f8a1a11fa4a38bad3f46acb
David Lerner <davidlernergm@gmail.com>
2025-02-06 18:19:22 +0200

    Initial commit: ASP.NET Core 8 API and Angular project setup

--- TaskManager.API/obj/Debug/net8.0/TaskManager.API.AssemblyInfo.cs
+++ TaskManager.API/obj/Debug/net8.0/TaskManager.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a83461438484b4e3ad271bdfbfd5cfc0baaedb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
